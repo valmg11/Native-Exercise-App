7fca820a2c3934d4524e40e2050956935e905079
valmg11 <tictactina05@gmail.com>
2026-04-05 21:22:55 -0400

    home buttons creation

--- native-exercise-app/App.js
+++ native-exercise-app/App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  let title = ["Exercise!", "Plank", "Running", "Push Ups", "Sit Ups", "Leg Press", "Chest Press"];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{title[0]}</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.container2}>
+        <Button 
+        color="red"
+        // style={styles.homeButtons}
+        title={title[1]}/>
+        <Button 
+        margin
+        style={styles.homeButtons}
+        title={title[2]}/>
+
+        <Button 
+        margin
+        style={styles.homeButtons}
+        title={title[3]}/>
+        <Button 
+        margin
+        style={styles.homeButtons}
+        title={title[4]}/>
+
+        <Button 
+        margin
+        style={styles.homeButtons}
+        title={title[5]}/>
+        <Button 
+        margin
+        style={styles.homeButtons}
+        title={title[6]}/>
+      </View>
+
     </View>
+    
   );
 }
 
@@ -16,5 +48,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  homeButtons: {
+    color: "red",
+    marginTop: 10,
   },
 });
